edf32057f1cd29b29f6daeb16d00b70ab833ea05
Bruce <3765629-lok.bruce@users.noreply.gitlab.com>
2021-09-15 04:46:49 +0000

    init commit tf codes

---
+++ main.tf
@@ -0,0 +1,22 @@
+# main.tf | Main Configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #      version = "2.70.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "ttt-state-bucket"
+    key    = "mykey/terraform_state.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
---
+++ vpc.tf
@@ -0,0 +1,13 @@
+# vpc.tf | VPC Configuration
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "${var.app_name}-vpc"
+    Environment = var.app_environment
+  }
+}
+
+
